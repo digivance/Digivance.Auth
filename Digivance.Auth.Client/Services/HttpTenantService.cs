207ac69418d3837dbc54c1f0fea77b268ecd39b1
Dan Mayor <dmayor@digivance.com>
2025-01-27 21:37:08 -0500

    #9 #11 Introducing Digivance.Auth.Client concept

---
+++ Digivance.Auth.Client/Services/HttpTenantService.cs
@@ -0,0 +1,48 @@
+using Digivance.Auth.Data.Commands;
+using Digivance.Auth.Data.Models;
+using Digivance.Auth.Data.Services;
+
+namespace Digivance.Auth.Client.Services
+{
+    /// <summary>
+    /// This implementation will make API calls to the Digivance.Auth.Api service to interact
+    /// with tenant records
+    /// </summary>
+    public class HttpTenantService : ITenantService
+    {
+        public Task<Tenant> CreateTenantAsync(CreateTenant command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTenantByIdAsync(Guid tenantId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tenant?> GetTenantByIdAsync(Guid tenantId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tenant?> GetTenantByNameAsync(string name, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TenantExistsAsync(Guid id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TenantNameExistsAsync(string name, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tenant> UpdateTenantAsync(Guid id, UpdateTenant command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Digivance.Auth.Client/Services/HttpUserService.cs
@@ -0,0 +1,53 @@
+﻿using Digivance.Auth.Data.Commands;
+using Digivance.Auth.Data.Models;
+using Digivance.Auth.Data.Services;
+
+namespace Digivance.Auth.Client.Services
+{
+    /// <summary>
+    /// This implementation will make API calls to the Digivance.Auth.Api service to interact
+    /// with user records
+    /// </summary>
+    public class HttpUserService : IUserService
+    {
+        public Task<UserAccount> CreateUserAsync(CreateUser command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserByIdAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EmailAddressTaken(string emailAddress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserAccount?> GetUserByEmailAddress(string emailAddress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserAccount?> GetUserByHandleAsync(string handle, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserAccount?> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserAccount> UpdateUserAsync(Guid id, UpdateUser command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
