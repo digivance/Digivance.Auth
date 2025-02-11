f1c23227c320d32dacd30b8535be316536d79e25
Dan Mayor <dmayor@digivance.com>
2025-02-10 20:38:42 -0500

    #45 - Introducing PermissionService (#67)

---
+++ src/Digivance.Auth.Data/Commands/UpdatePermission.cs
@@ -0,0 +1,13 @@
+﻿namespace Digivance.Auth.Data.Commands
+{
+    /// <summary>
+    /// Command to update a permission record
+    /// </summary>
+    public record UpdatePermission
+    {
+        /// <summary>
+        /// Human friendly description of this permission
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
