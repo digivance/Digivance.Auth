f00cf289490df0b34449051791233cd5ba81f96b
DigiGunjayas <136254626+DigiGunjayas@users.noreply.github.com>
2025-02-26 02:02:07 +0545

    update tenant validator test (#82)

---
+++ src/Digivance.Auth.Data.Tests/Commands/UpdateTenantTests.cs
@@ -0,0 +1,152 @@
+﻿using Digivance.Auth.Data.Commands;
+using Digivance.Auth.Data.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digivance.Auth.Data.Tests.Commands
+{
+    [TestFixture]
+    public class UpdateTenantTests
+    {
+        /// <summary>
+        /// Helper method to build a mock ITenantService. Will return returnsTrue when you call
+        /// ExistsByNameAsync
+        /// </summary>
+        /// <param name="returnsTrue">The value to return for ExistsByNameAsync</param>
+        /// <returns>A mock implementation of ITenantService</returns>
+        private ITenantService GetTenantService(bool returnsTrue, bool returnsTrue1)
+        {
+            var mock = new Mock<ITenantService>();
+
+            mock
+                .Setup(x => x.ExistsByNameAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(returnsTrue));
+
+            mock
+                .Setup(x => x.ExistsAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult(returnsTrue1));
+
+            return mock.Object;
+        }
+
+        [Test]
+        public async Task Can_BeValid()
+        {
+            var service = GetTenantService(false, true);
+            var validator = new UpdateTenantValidator(service);
+
+            var command = new UpdateTenant
+            {
+                Description = "Valid description",
+                Name = "Irrelevant to this test"
+            };
+
+            var res = await validator.ValidateAsync(command, default);
+
+            Assert.That(res.IsValid, Is.True);
+        }
+
+        [Test]
+        public async Task CanFail_LongDescription()
+        {
+            var service = GetTenantService(false, true);
+            var validator = new UpdateTenantValidator(service);
+
+            var description = "";
+            for (var i = 0; i <= 4001; i++)
+                description += "I'm a long string, too long one might say...";
+
+            var command = new UpdateTenant
+            {
+                Description = description,
+                Name = "Irrelevant to this test"
+            };
+
+            var res = await validator.ValidateAsync(command, default);
+
+            Assert.That(res.IsValid, Is.False);
+            Assert.That(res.Errors.All(x => x.ErrorMessage == UpdateTenantValidator.ERR_DESCRIPTION_LENGTH), Is.True);
+        }
+
+        [Test]
+        public async Task CanFail_LongName()
+        {
+            var service = GetTenantService(false, true);
+            var validator = new UpdateTenantValidator(service);
+
+            var name = "";
+            for (var i = 0; i <= 101; i++)
+                name += "I'm a long string, too long but not comparatively atleast...";
+
+            var command = new UpdateTenant
+            {
+                Description = "Irrelevant to this test",
+                Name = name
+            };
+
+            var res = await validator.ValidateAsync(command, default);
+
+            Assert.That(res.IsValid, Is.False);
+            Assert.That(res.Errors.All(x => x.ErrorMessage == UpdateTenantValidator.ERR_TENANTNAME_LENGTH), Is.True);
+        }
+
+        [Test]
+        public async Task CanFail_MissingName()
+        {
+            var service = GetTenantService(false, true);
+            var validator = new UpdateTenantValidator(service);
+
+            var command = new UpdateTenant
+            {
+                Description = "Valid description",
+                Name = ""
+            };
+
+            var res = await validator.ValidateAsync(command, default);
+
+            Assert.That(res.IsValid, Is.False);
+            Assert.That(res.Errors.All(x => x.ErrorMessage == "'Name' must not be empty."), Is.True);
+        }
+
+        [Test]
+        public async Task CanFail_DuplicateName()
+        {
+            // This (tenant service) will say the tenant already exists which should
+            // cause our validator to fail
+            var service = GetTenantService(true, true);
+            var validator = new UpdateTenantValidator(service);
+
+            var command = new UpdateTenant
+            {
+                Name = "Already Exists"
+            };
+
+            var res = await validator.ValidateAsync(command, default);
+
+            Assert.That(res.IsValid, Is.False);
+            Assert.That(res.Errors.All(x => x.ErrorMessage == UpdateTenantValidator.ERR_TENANTNAME_UNIQUE), Is.True);
+        }
+
+        [Test]
+        public async Task CanFail_IDNotExists()
+        {
+            var service = GetTenantService(false, false);
+            var validator = new UpdateTenantValidator(service);
+
+            var command = new UpdateTenant
+            {
+                Name = "Already Exists",
+                Id = Guid.NewGuid()
+            };
+
+            var res = await validator.ValidateAsync(command, default);
+
+            Assert.That(res.IsValid, Is.False);
+            Assert.That(res.Errors.All(x => x.ErrorMessage == UpdateTenantValidator.ERR_TENANTID_NOTFOUND), Is.True);
+        }
+    }
+}
--- src/Digivance.Auth.Data/Commands/UpdateTenant.cs
+++ src/Digivance.Auth.Data/Commands/UpdateTenant.cs
@@ -1,6 +1,81 @@
-﻿namespace Digivance.Auth.Data.Commands
+﻿using Digivance.Auth.Data.Services;
+using FluentValidation;
+
+namespace Digivance.Auth.Data.Commands
 {
-    public record UpdateTenant : CreateTenant
+    public record UpdateTenant
     {
+        /// <summary>
+        /// Unique id of the tenant we want to update
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// User friendly description of this tenant
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// User friendly name of this tenant
+        /// </summary>
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// Fluent validations for our UpdateTenant command
+    /// </summary>
+    public class UpdateTenantValidator : AbstractValidator<UpdateTenant>
+    {
+        public const string ERR_DESCRIPTION_LENGTH = "Description must be 4000 characters or less";
+        public const string ERR_TENANTID_NOTFOUND = "No tenant found for this tenant id";
+        public const string ERR_TENANTNAME_LENGTH = "Tenant name must be 100 characters or less";
+        public const string ERR_TENANTNAME_UNIQUE = "An Tenant already exists with this name";
+
+        private readonly ITenantService tenantService;
+
+        /// <summary>
+        /// Standard constructor
+        /// </summary>
+        /// <param name="tenantService">The tenantService service to validate with</param>
+        public UpdateTenantValidator(ITenantService tenantService)
+        {
+            this.tenantService = tenantService;
+
+            // Description must be 4k or less chars if provided (can be null or empty)
+            RuleFor(x => x.Description)
+                .MaximumLength(4000)
+                    .WithMessage(ERR_DESCRIPTION_LENGTH);
+
+            // Tenant with this Id must exists
+            RuleFor(x => x.Id)
+                .MustAsync(BeExistingTenant)
+                    .WithMessage(ERR_TENANTID_NOTFOUND);
+
+            // Name must be provided and be unique
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100)
+                    .WithMessage(ERR_TENANTNAME_LENGTH)
+                .MustAsync(BeUniqueTenantname)
+                    .WithMessage(ERR_TENANTNAME_UNIQUE);
+        }
+
+        /// <summary>
+        /// Checks to see if this Tenantname is already in use
+        /// </summary>
+        /// <param name="tenantname">The Tenantname to check</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>True if this is a unique Tenantname</returns>
+        public async Task<bool> BeUniqueTenantname(string tenantname, CancellationToken cancellationToken)
+            => !await tenantService.ExistsByNameAsync(tenantname, cancellationToken);
+
+        /// <summary>
+        /// Ensures the provided tenant id exists
+        /// </summary>
+        /// <param name="tenantId">Unique id of the tenant we are validating this command for</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>True if this tenant exists</returns>
+        public Task<bool> BeExistingTenant(Guid tenantId, CancellationToken cancellationToken)
+            => tenantService.ExistsAsync(tenantId, cancellationToken);
     }
 }
