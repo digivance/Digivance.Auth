ea2a47d0181af1eccbf26ae75d117825d9f1023e
DigiGunjayas <136254626+DigiGunjayas@users.noreply.github.com>
2025-02-27 00:09:39 +0545

    scope command tests (#83)
    * scope command tests
    
    * fix
    
    * fix
    
    * fix again

--- src/Digivance.Auth.Data.EntityFramework/Services/EfTenantService.cs
+++ src/Digivance.Auth.Data.EntityFramework/Services/EfTenantService.cs
@@ -64,10 +64,21 @@ namespace Digivance.Auth.Data.EntityFramework.Services
         }
 
         /// <inheritdoc />
-        public Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken)
-            => context.Tenants
-            .Where(x => x.Id == id)
-            .AnyAsync(cancellationToken);
+        public Task<bool> ExistsAsync(Guid? id, CancellationToken cancellationToken)
+        {
+            if (id == null)
+            {
+                return Task.FromResult(true);
+            }
+            else
+            {
+                var exists = context.Tenants
+                    .Where(x => x.Id == id)
+                    .AnyAsync(cancellationToken);
+
+                return exists;
+            }
+        }
 
         /// <inheritdoc />
         public Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken)
